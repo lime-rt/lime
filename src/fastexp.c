9260408c8fc6b1ef3737ace7d8d6d0e0dc4c03ce
Sebastien Maret <sebastien.maret@obs.ujf-grenoble.fr>
2015-07-16 11:24:40 +0200

    Update the copyright notice in all source files

--- src/fastexp.c
+++ src/fastexp.c
@@ -1,3 +1,12 @@
+ /*
+ *  fastexp.c
+ *  This file is part of LIME, the versatile line modeling engine
+ *
+ *  Copyright (C) 2006-2014 Christian Brinch
+ *  Copyright (C) 2015 The LIME development team
+ *
+ */
+
 #include "lime.h"
 
 
