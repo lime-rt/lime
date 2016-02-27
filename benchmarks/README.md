2dbf2ed8b3287878e8351b85e9e5495fe0425268
Sebastien Maret <sebastien.maret@obs.ujf-grenoble.fr>
2016-02-27 17:23:47 +0100

    Add a benchmark model
    This model correspond to model 1a and 1b from the van Zadelhoff (2002)
    paper.

---
+++ benchmarks/README.md
@@ -0,0 +1,9 @@
+Benchmarks models for LIME
+==========================
+
+This directory contains several benchmark models for LIME. These models are adapted from [van Zadelhoff et al., Astronomy and Astrophysics, 395, p.373-384 (2002)](http://dx.doi.org/10.1051/0004-6361:20021226).
+
+Models 1a/1b
+------------
+
+This model is a simple 2-level molecule problem for a 1D static cloud in spherical geometry. The density is assumed to vary as a power law, while the temperature is assumed to be constant. Models 1a and 1b differ only by the assumed abundance of the molecule. Model 1a has a moderate optical depth (τ ≃ 60 at the line center). Model 1b has a much larger opacity (τ ≃ 4800).
---
+++ benchmarks/model1/model1b.c
@@ -0,0 +1,74 @@
+/*
+ *  model1b.c -- Benchmark model for LIME
+ *
+ *  This file is part of LIME, the versatile line modeling engine
+ *
+ *  Copyright (C) 2006-2014 Christian Brinch
+ *  Copyright (C) 2015-2016 The LIME development team
+ *
+ */
+
+#include "lime.h"
+
+void
+input (inputPars * par, image * img)
+{
+  par->radius = 7.8e16;
+  par->minScale = 1e13;
+  par->pIntensity = 4000;
+  par->sinkPoints = 3000;
+  par->dust = NULL;
+  par->moldatfile[0] = "molec.dat";
+  par->antialias = 1;
+  par->sampling = 2;
+  par->outputfile = "model1b.pop";
+
+  img[0].nchan = 32;
+  img[0].velres = 100.;
+  img[0].trans = 0;
+  img[0].pxls = 128;
+  img[0].imgres = 0.1;
+  img[0].theta = 0.0;
+  img[0].distance = 140 * PC;
+  img[0].source_vel = 0;
+  img[0].unit = 0;
+  img[0].filename = "model1b.fits";
+}
+
+void
+density (double x, double y, double z, double *density)
+{
+  double r;
+
+  r = sqrt (x * x + y * y + z * z);
+  if ((r >= 1e13) && (r <= 7.8e16))
+    density[0] = 2e7 * pow (r / 1e13, -2) * 1e6;
+  else
+    density[0] = 0.;
+}
+
+void
+temperature (double x, double y, double z, double *temperature)
+{
+  temperature[0] = 20.;
+}
+
+void
+abundance (double x, double y, double z, double *abundance)
+{
+  abundance[0] = 1e-6;
+}
+
+void
+doppler (double x, double y, double z, double *doppler)
+{
+  *doppler = 150.;
+}
+
+void
+velocity (double x, double y, double z, double *vel)
+{
+  vel[0] = 0;
+  vel[1] = 0;
+  vel[2] = 0;
+}
