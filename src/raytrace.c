7b5bf5d2b88f6bd572c392deb319db084acfb487
ims <ims@strw.leidenuniv.nl>
2016-04-08 16:51:37 +0200

    Rearranged if-block in traceray().
    There is a test near the start of the function to see whether the ray will intersect the
    model sphere at all; if so, the following large block of ~110 statements (containing the
    entire functionality of the function) is entered. It is obviously much less spendthrift
    of indents to simply exit the function if the ray does _not_ pass through the model
    sphere. The code has been rearranged to implement this change. All the statements which
    were formerly within the block have been unindented by 2 spaces; there are no further
    changes in the present commit.

--- src/raytrace.c
+++ src/raytrace.c
@@ -87,111 +87,114 @@ Note that the algorithm employed here is similar to that employed in the functio
   xp=ray.x;
   yp=ray.y;
 
-  if((xp*xp+yp*yp)/par->radiusSqu <= 1 ) {
-    zp=-sqrt(par->radiusSqu-(xp*xp+yp*yp)); /* There are two points of intersection between the line of sight and the spherical model surface; this is the Z coordinate (in the unrotated frame) of the one nearer to the observer. */
+  /* The model is circular in projection. We only follow the ray if it will intersect the model.
+  */
+  if((xp*xp+yp*yp)>par->radiusSqu)
+    return;
 
-    /* Rotate the line of sight as desired. */
-    for(i=0;i<3;i++){
-      x[i]=xp*img[im].rotMat[i][0] + yp*img[im].rotMat[i][1] + zp*img[im].rotMat[i][2];
-      dx[i]= img[im].rotMat[i][2]; /* This points away from the observer. */
-    }
+  zp=-sqrt(par->radiusSqu-(xp*xp+yp*yp)); /* There are two points of intersection between the line of sight and the spherical model surface; this is the Z coordinate (in the unrotated frame) of the one nearer to the observer. */
 
-    contMolI = 0; /****** Always?? */
-
-    if(img[im].doline && img[im].trans > -1)
-      contLineI = img[im].trans;
-    else if(img[im].doline && img[im].trans == -1)
-      contLineI = tmptrans;
-    else
-      contLineI = 0;
-
-    /* Find the grid point nearest to the starting x. */
-    i=0;
-    dist2=(x[0]-g[i].x[0])*(x[0]-g[i].x[0]) + (x[1]-g[i].x[1])*(x[1]-g[i].x[1]) + (x[2]-g[i].x[2])*(x[2]-g[i].x[2]);
-    posn=i;
-    for(i=1;i<par->ncell;i++){
-      ndist2=(x[0]-g[i].x[0])*(x[0]-g[i].x[0]) + (x[1]-g[i].x[1])*(x[1]-g[i].x[1]) + (x[2]-g[i].x[2])*(x[2]-g[i].x[2]);
-      if(ndist2<dist2){
-        posn=i;
-        dist2=ndist2;
-      }
-    }
-
-    col=0;
-    do{
-      ds=-2.*zp-col; /* This default value is chosen to be as large as possible given the spherical model boundary. */
-      nposn=-1;
-      line_plane_intersect(g,&ds,posn,&nposn,dx,x,cutoff); /* Returns a new ds equal to the distance to the next Voronoi face, and nposn, the ID of the grid cell that abuts that face. */ 
-      if(par->polarization){
-        for(ichan=0;ichan<img[im].nchan;ichan++){
-          sourceFunc_pol(ds, g[posn].B, m[0], gAux[posn].mol[0], 0, img[im].theta, snu_pol, &dtau);
-#ifdef FASTEXP
-          ray.intensity[ichan]+=FastExp(ray.tau[ichan])*(1.-exp(-dtau))*snu_pol[ichan];
-#else
-          ray.intensity[ichan]+=   exp(-ray.tau[ichan])*(1.-exp(-dtau))*snu_pol[ichan];
-#endif
-          ray.tau[ichan]+=dtau;
-        }
-      } else {
-        for(ichan=0;ichan<img[im].nchan;ichan++){
-          jnu=.0;
-          alpha=0.;
-
-          for(iline=0;iline<nlinetot;iline++){
-            molI = counta[iline];
-            lineI = countb[iline];
-            if(img[im].doline && m[molI].freq[lineI] > img[im].freq-img[im].bandwidth/2.
-            && m[molI].freq[lineI] < img[im].freq+img[im].bandwidth/2.){
-              /* Calculate the red shift of the transition wrt to the frequency specified for the image. */
-              if(img[im].trans > -1){
-                lineRedShift=(m[molI].freq[img[im].trans]-m[molI].freq[lineI])/m[molI].freq[img[im].trans]*CLIGHT;
-              } else {
-                lineRedShift=(img[im].freq-m[molI].freq[lineI])/img[im].freq*CLIGHT;
-              }
-
-              vThisChan=(ichan-(img[im].nchan-1)/2.)*img[im].velres; /* Consistent with the WCS definition in writefits(). */
-              deltav = vThisChan - img[im].source_vel - lineRedShift;
-              /* Line centre occurs when deltav = the recession velocity of the radiating material. Explanation of the signs of the 2nd and 3rd terms on the RHS: (i) A bulk source velocity (which is defined as >0 for the receding direction) should be added to the material velocity field; this is equivalent to subtracting it from deltav, as here. (ii) A positive value of lineRedShift means the line is red-shifted wrt to the frequency specified for the image. The effect is the same as if the line and image frequencies were the same, but the bulk recession velocity were higher. lineRedShift should thus be added to the recession velocity, which is equivalent to subtracting it from deltav, as here. */
-
-              /* Calculate an approximate average line-shape function at deltav within the Voronoi cell. */
-              if(!par->pregrid) calcLineAmpSample(x,dx,ds,g[posn].mol[molI].binv,deltav,&vfac);
-              else vfac=gaussline(deltav+veloproject(dx,g[posn].vel),g[posn].mol[molI].binv);
-
-              /* Increment jnu and alpha for this Voronoi cell by the amounts appropriate to the spectral line. */
-              sourceFunc_line(m[molI], vfac, g[posn].mol[molI], lineI, &jnu, &alpha);
-            }
-          }
-
-          sourceFunc_cont(g[posn].mol[contMolI], contLineI, &jnu, &alpha);
-          dtau=alpha*ds;
-          calcSourceFn(dtau, par, &remnantSnu, &expDTau);
-          remnantSnu *= jnu*m[0].norminv*ds;
-#ifdef FASTEXP
-          ray.intensity[ichan]+=FastExp(ray.tau[ichan])*remnantSnu;
-#else
-          ray.intensity[ichan]+=   exp(-ray.tau[ichan])*remnantSnu;
-#endif
-          ray.tau[ichan]+=dtau;
-        }
-      }
-
-      /* Move the working point to the edge of the next Voronoi cell. */
-      for(i=0;i<3;i++) x[i]+=ds*dx[i];
-      col+=ds;
-      posn=nposn;
-    } while(col < 2.0*fabs(zp));
-
-    /* Add or subtract cmb. */
-#ifdef FASTEXP
-    for(ichan=0;ichan<img[im].nchan;ichan++){
-      ray.intensity[ichan]+=FastExp(ray.tau[ichan])*m[0].local_cmb[tmptrans];
-    }
-#else
-    for(ichan=0;ichan<img[im].nchan;ichan++){
-      ray.intensity[ichan]+=exp(-ray.tau[ichan])*m[0].local_cmb[tmptrans];
-    }
-#endif
+  /* Rotate the line of sight as desired. */
+  for(i=0;i<3;i++){
+    x[i]=xp*img[im].rotMat[i][0] + yp*img[im].rotMat[i][1] + zp*img[im].rotMat[i][2];
+    dx[i]= img[im].rotMat[i][2]; /* This points away from the observer. */
   }
+
+  contMolI = 0; /****** Always?? */
+
+  if(img[im].doline && img[im].trans > -1)
+    contLineI = img[im].trans;
+  else if(img[im].doline && img[im].trans == -1)
+    contLineI = tmptrans;
+  else
+    contLineI = 0;
+
+  /* Find the grid point nearest to the starting x. */
+  i=0;
+  dist2=(x[0]-g[i].x[0])*(x[0]-g[i].x[0]) + (x[1]-g[i].x[1])*(x[1]-g[i].x[1]) + (x[2]-g[i].x[2])*(x[2]-g[i].x[2]);
+  posn=i;
+  for(i=1;i<par->ncell;i++){
+    ndist2=(x[0]-g[i].x[0])*(x[0]-g[i].x[0]) + (x[1]-g[i].x[1])*(x[1]-g[i].x[1]) + (x[2]-g[i].x[2])*(x[2]-g[i].x[2]);
+    if(ndist2<dist2){
+      posn=i;
+      dist2=ndist2;
+    }
+  }
+
+  col=0;
+  do{
+    ds=-2.*zp-col; /* This default value is chosen to be as large as possible given the spherical model boundary. */
+    nposn=-1;
+    line_plane_intersect(g,&ds,posn,&nposn,dx,x,cutoff); /* Returns a new ds equal to the distance to the next Voronoi face, and nposn, the ID of the grid cell that abuts that face. */ 
+    if(par->polarization){
+      for(ichan=0;ichan<img[im].nchan;ichan++){
+        sourceFunc_pol(ds, g[posn].B, m[0], gAux[posn].mol[0], 0, img[im].theta, snu_pol, &dtau);
+#ifdef FASTEXP
+        ray.intensity[ichan]+=FastExp(ray.tau[ichan])*(1.-exp(-dtau))*snu_pol[ichan];
+#else
+        ray.intensity[ichan]+=   exp(-ray.tau[ichan])*(1.-exp(-dtau))*snu_pol[ichan];
+#endif
+        ray.tau[ichan]+=dtau;
+      }
+    } else {
+      for(ichan=0;ichan<img[im].nchan;ichan++){
+        jnu=.0;
+        alpha=0.;
+
+        for(iline=0;iline<nlinetot;iline++){
+          molI = counta[iline];
+          lineI = countb[iline];
+          if(img[im].doline && m[molI].freq[lineI] > img[im].freq-img[im].bandwidth/2.
+          && m[molI].freq[lineI] < img[im].freq+img[im].bandwidth/2.){
+            /* Calculate the red shift of the transition wrt to the frequency specified for the image. */
+            if(img[im].trans > -1){
+              lineRedShift=(m[molI].freq[img[im].trans]-m[molI].freq[lineI])/m[molI].freq[img[im].trans]*CLIGHT;
+            } else {
+              lineRedShift=(img[im].freq-m[molI].freq[lineI])/img[im].freq*CLIGHT;
+            }
+
+            vThisChan=(ichan-(img[im].nchan-1)/2.)*img[im].velres; /* Consistent with the WCS definition in writefits(). */
+            deltav = vThisChan - img[im].source_vel - lineRedShift;
+            /* Line centre occurs when deltav = the recession velocity of the radiating material. Explanation of the signs of the 2nd and 3rd terms on the RHS: (i) A bulk source velocity (which is defined as >0 for the receding direction) should be added to the material velocity field; this is equivalent to subtracting it from deltav, as here. (ii) A positive value of lineRedShift means the line is red-shifted wrt to the frequency specified for the image. The effect is the same as if the line and image frequencies were the same, but the bulk recession velocity were higher. lineRedShift should thus be added to the recession velocity, which is equivalent to subtracting it from deltav, as here. */
+
+            /* Calculate an approximate average line-shape function at deltav within the Voronoi cell. */
+            if(!par->pregrid) calcLineAmpSample(x,dx,ds,g[posn].mol[molI].binv,deltav,&vfac);
+            else vfac=gaussline(deltav+veloproject(dx,g[posn].vel),g[posn].mol[molI].binv);
+
+            /* Increment jnu and alpha for this Voronoi cell by the amounts appropriate to the spectral line. */
+            sourceFunc_line(m[molI], vfac, g[posn].mol[molI], lineI, &jnu, &alpha);
+          }
+        }
+
+        sourceFunc_cont(g[posn].mol[contMolI], contLineI, &jnu, &alpha);
+        dtau=alpha*ds;
+        calcSourceFn(dtau, par, &remnantSnu, &expDTau);
+        remnantSnu *= jnu*m[0].norminv*ds;
+#ifdef FASTEXP
+        ray.intensity[ichan]+=FastExp(ray.tau[ichan])*remnantSnu;
+#else
+        ray.intensity[ichan]+=   exp(-ray.tau[ichan])*remnantSnu;
+#endif
+        ray.tau[ichan]+=dtau;
+      }
+    }
+
+    /* Move the working point to the edge of the next Voronoi cell. */
+    for(i=0;i<3;i++) x[i]+=ds*dx[i];
+    col+=ds;
+    posn=nposn;
+  } while(col < 2.0*fabs(zp));
+
+  /* Add or subtract cmb. */
+#ifdef FASTEXP
+  for(ichan=0;ichan<img[im].nchan;ichan++){
+    ray.intensity[ichan]+=FastExp(ray.tau[ichan])*m[0].local_cmb[tmptrans];
+  }
+#else
+  for(ichan=0;ichan<img[im].nchan;ichan++){
+    ray.intensity[ichan]+=exp(-ray.tau[ichan])*m[0].local_cmb[tmptrans];
+  }
+#endif
 }
 
 
