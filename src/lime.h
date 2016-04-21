43f974907ed24aef3e101ac64f28609ae61482e1
ims <ims@strw.leidenuniv.nl>
2016-04-21 11:13:13 +0200

    Added warning if density too low
    Problem #85 seems to occur when the density function returns a very low (much lower than
    physically plausible) value for some grid point location. Even though the previous
    commit introduced a plan B to handle such cases nicely, ideally the user would avoid the
    situation arising in the first place by defining a floor in their density() function.
    The present change prints a warning when the density at any (internal) grid point is
    found to be less than a nominal value of 1000 which has been provided via a macro
    TYPICAL_ISM_DENS in lime.h.
    
    Note that the warning takes 2 lines, so will not print properly if curses output is
    selected. Yeah, well...

--- src/lime.h
+++ src/lime.h
@@ -60,6 +60,7 @@
 #define maxp		0.15
 #define OtoP		3.
 #define GRAV		6.67428e-11
+#define TYPICAL_ISM_DENS	1000.0
 
 /* Other constants */
 #define NITERATIONS 	16
@@ -189,6 +190,7 @@ void gasIIdust(double,double,double,double *);
 void   	binpopsout(inputPars *, struct grid *, molData *);
 void   	buildGrid(inputPars *, struct grid *);
 void    calcSourceFn(double dTau, const inputPars *par, double *remnantSnu, double *expDTau);
+void	checkGridDensities(inputPars *par, struct grid *g);
 void	continuumSetup(int, image *, molData *, inputPars *, struct grid *);
 void	distCalc(inputPars *, struct grid *);
 void	fit_d1fi(double, double, double*);
