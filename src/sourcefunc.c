e21f8e38cf512af529ecfac402ada2e7feea6e1e
ims <ims@strw.leidenuniv.nl>
2016-05-20 12:43:11 +0200

    Fixed error in expression for Stokes I.
    As pointed out by Sergey Parfenov, the expression for Stokes I was taken from a
    paper which has been subsequently shown to be wrong by a factor of 2. This has now
    been corrected here.

--- src/sourcefunc.c
+++ src/sourcefunc.c
@@ -72,11 +72,11 @@ sourceFunc_cont(double *jnu, double *alpha,struct grid *g,int pos,int ispec, int
 
 void sourceFunc_pol(double *snu, double *alpha, struct grid *g, int pos, int ispec, int iline, double (*rotMat)[3]){
   /*
-The theory behind this is drawn from
+The theory behind this was originally drawn from
 
   Padovani, M. et al, A&A 543, A16 (2012)
 
-and references therein.
+and references therein. However, as pointed out in Ade, P. A. R. et al, A&A 576, A105 (2015), Padovani's expression for sigma2 is too small by a factor of 2. This correction has been propagated here.
   */
   double jnu, trigFuncs[3];
   
@@ -85,7 +85,7 @@ and references therein.
   /* Emission */
   /* Continuum part:	j_nu = rho_dust * kappa_nu */
   jnu = g[pos].mol[ispec].dust[iline]*g[pos].mol[ispec].knu[iline];
-  snu[0] = jnu*(1.0 - maxp*(0.5*trigFuncs[0] - 1.0/3.0));
+  snu[0] = jnu*(1.0 - maxp*(trigFuncs[0] - 2.0/3.0));
   snu[1] = jnu*maxp*trigFuncs[1]*trigFuncs[0];
   snu[2] = jnu*maxp*trigFuncs[2]*trigFuncs[0];
   
