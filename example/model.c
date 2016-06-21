2a553f9d29e199c0dff5308885f98cb09af20c21
ims <ims@strw.leidenuniv.nl>
2016-06-21 14:59:48 +0200

    Cleaned up model.c
    The model.c file is replete with tabs which I have replaced by strings of single spaces. The
    reason for this is that tabs have different widths in different editors/platforms: the
    result can look messy and can confuse users who are more used to python.
    
    I also replaced the 0 in the img index by 'i' which makes cut-n-paste errors less likely
    when users are adding 2nd or further images to their own model.c files.

--- example/model.c
+++ example/model.c
@@ -13,35 +13,38 @@
 
 void
 input(inputPars *par, image *img){
+  int i;
+
   /*
    * Basic parameters. See cheat sheet for details.
    */
-  par->radius			= 2000*AU;
-  par->minScale	   		= 0.5*AU;
-  par->pIntensity    	= 4000;
-  par->sinkPoints    	= 3000;
-  par->dust				= "jena_thin_e6.tab";
-  par->moldatfile[0] 	= "hco+@xpol.dat";
-  par->antialias		= 4;
-  par->sampling			= 2; // log distr. for radius, directions distr. uniformly on a sphere.
+  par->radius                   = 2000*AU;
+  par->minScale                 = 0.5*AU;
+  par->pIntensity               = 4000;
+  par->sinkPoints               = 3000;
+  par->dust                     = "jena_thin_e6.tab";
+  par->moldatfile[0]            = "hco+@xpol.dat";
+  par->antialias                = 4;
+  par->sampling                 = 2; // log distr. for radius, directions distr. uniformly on a sphere.
 
-  par->outputfile 		= "populations.pop";
-  par->binoutputfile 	= "restart.pop";
-  par->gridfile			= "grid.vtk";
+  par->outputfile               = "populations.pop";
+  par->binoutputfile            = "restart.pop";
+  par->gridfile                 = "grid.vtk";
 
   /*
-   * Definitions for image #0. Add blocks for additional images.
+   * Definitions for image #0. Add blocks with successive values of i for additional images.
    */
-  img[0].nchan			= 61;		  // Number of channels
-  img[0].velres			= 500.;       // Channel resolution in m/s
-  img[0].trans			= 3;          // zero-indexed J quantum number
-  img[0].pxls			= 100;	      // Pixels per dimension
-  img[0].imgres			= 0.1;		  // Resolution in arc seconds
-  img[0].theta			= 0.0;		  // 0: face-on, pi/2: edge-on
-  img[0].distance		= 140*PC;	  // source distance in m
-  img[0].source_vel		= 0;          // source velocity in m/s
-  img[0].unit			= 0;		  // 0:Kelvin 1:Jansky/pixel 2:SI 3:Lsun/pixel 4:tau
-  img[0].filename		= "image0.fits";	// Output filename
+  i=0;
+  img[i].nchan                  = 61;             // Number of channels
+  img[i].velres                 = 500.;           // Channel resolution in m/s
+  img[i].trans                  = 3;              // zero-indexed J quantum number
+  img[i].pxls                   = 100;            // Pixels per dimension
+  img[i].imgres                 = 0.1;            // Resolution in arc seconds
+  img[i].theta                  = 0.0;            // 0: face-on, pi/2: edge-on
+  img[i].distance               = 140*PC;         // source distance in m
+  img[i].source_vel             = 0;              // source velocity in m/s
+  img[i].unit                   = 0;              // 0:Kelvin 1:Jansky/pixel 2:SI 3:Lsun/pixel 4:tau
+  img[i].filename               = "image0.fits";  // Output filename
 }
 
 /******************************************************************************/
