7fa89ba7aa870743fbd5f37251c46b7299dd827b
Sebastien Maret <sebastien.maret@obs.ujf-grenoble.fr>
2016-09-12 17:29:04 +0200

    Update copyright notice

--- src/aux.c
+++ src/aux.c
@@ -3,7 +3,7 @@
  *  This file is part of LIME, the versatile line modeling engine
  *
  *  Copyright (C) 2006-2014 Christian Brinch
- *  Copyright (C) 2016 The LIME development team
+ *  Copyright (C) 2015-2016 The LIME development team
  *
 TODO:
   - The test to run photon() etc in levelPops just tests dens[0]. This is a bit sloppy.
